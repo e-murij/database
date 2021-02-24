a0090efc16a1fffad570895b2d5250f93b2c6aaa
e.murij <murij.kat@gmail.com>
2021-02-25 00:23:32 +0700

    codepage fix

--- lesson_5.sql
+++ lesson_5.sql
@@ -1,49 +1,49 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255) COMMENT 'Имя покупателя',
-  birthday_at DATE COMMENT 'Дата рождения',
+  name VARCHAR(255) COMMENT 'РРјСЏ РїРѕРєСѓРїР°С‚РµР»СЏ',
+  birthday_at DATE COMMENT 'Р”Р°С‚Р° СЂРѕР¶РґРµРЅРёСЏ',
   created_at DATETIME,
   updated_at DATETIME
-) COMMENT = 'Покупатели';
+) COMMENT = 'РџРѕРєСѓРїР°С‚РµР»Рё';
 
 INSERT INTO
   users (name, birthday_at, created_at, updated_at)
 VALUES
-  ('Геннадий', '1990-10-05', NULL, NULL),
-  ('Наталья', '1984-11-12', NULL, NULL),
-  ('Александр', '1985-05-20', NULL, NULL),
-  ('Сергей', '1988-02-14', NULL, NULL),
-  ('Иван', '1998-01-12', NULL, NULL),
-  ('Мария', '2006-08-29', NULL, NULL);
+  ('Р“РµРЅРЅР°РґРёР№', '1990-10-05', NULL, NULL),
+  ('РќР°С‚Р°Р»СЊСЏ', '1984-11-12', NULL, NULL),
+  ('РђР»РµРєСЃР°РЅРґСЂ', '1985-05-20', NULL, NULL),
+  ('РЎРµСЂРіРµР№', '1988-02-14', NULL, NULL),
+  ('РРІР°РЅ', '1998-01-12', NULL, NULL),
+  ('РњР°СЂРёСЏ', '2006-08-29', NULL, NULL);
 
--- Тема Операции, задание 1
+-- РўРµРјР° РћРїРµСЂР°С†РёРё, Р·Р°РґР°РЅРёРµ 1
 
 UPDATE users SET created_at = NOW(); 
 UPDATE users SET updated_at = NOW(); 
 
 SELECT * FROM users;
 
--- Тема Операции, задание 2
+-- РўРµРјР° РћРїРµСЂР°С†РёРё, Р·Р°РґР°РЅРёРµ 2
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255) COMMENT 'Имя покупателя',
-  birthday_at DATE COMMENT 'Дата рождения',
+  name VARCHAR(255) COMMENT 'РРјСЏ РїРѕРєСѓРїР°С‚РµР»СЏ',
+  birthday_at DATE COMMENT 'Р”Р°С‚Р° СЂРѕР¶РґРµРЅРёСЏ',
   created_at VARCHAR(255),
   updated_at VARCHAR(255)
-) COMMENT = 'Покупатели';
+) COMMENT = 'РџРѕРєСѓРїР°С‚РµР»Рё';
 
 INSERT INTO
   users (name, birthday_at, created_at, updated_at)
 VALUES
-  ('Геннадий', '1990-10-05', '07.01.2016 12:05', '07.01.2016 12:05'),
-  ('Наталья', '1984-11-12', '20.05.2016 16:32', '20.05.2016 16:32'),
-  ('Александр', '1985-05-20', '14.08.2016 20:10', '14.08.2016 20:10'),
-  ('Сергей', '1988-02-14', '21.10.2016 9:14', '21.10.2016 9:14'),
-  ('Иван', '1998-01-12', '15.12.2016 12:45', '15.12.2016 12:45'),
-  ('Мария', '2006-08-29', '12.01.2017 8:56', '12.01.2017 8:56');
+  ('Р“РµРЅРЅР°РґРёР№', '1990-10-05', '07.01.2016 12:05', '07.01.2016 12:05'),
+  ('РќР°С‚Р°Р»СЊСЏ', '1984-11-12', '20.05.2016 16:32', '20.05.2016 16:32'),
+  ('РђР»РµРєСЃР°РЅРґСЂ', '1985-05-20', '14.08.2016 20:10', '14.08.2016 20:10'),
+  ('РЎРµСЂРіРµР№', '1988-02-14', '21.10.2016 9:14', '21.10.2016 9:14'),
+  ('РРІР°РЅ', '1998-01-12', '15.12.2016 12:45', '15.12.2016 12:45'),
+  ('РњР°СЂРёСЏ', '2006-08-29', '12.01.2017 8:56', '12.01.2017 8:56');
   
 UPDATE users SET created_at = STR_TO_DATE(created_at, "%d.%m.%Y %k:%i");
 UPDATE users SET updated_at = STR_TO_DATE(updated_at , "%d.%m.%Y %k:%i");
@@ -57,17 +57,17 @@ SELECT * FROM users;
 SHOW COLUMNS FROM users;
 
 
--- Тема Операции, задание 3
+-- РўРµРјР° РћРїРµСЂР°С†РёРё, Р·Р°РґР°РЅРёРµ 3
 
 DROP TABLE IF EXISTS storehouses_products;
 CREATE TABLE storehouses_products (
   id SERIAL PRIMARY KEY,
   storehouse_id INT UNSIGNED,
   product_id INT UNSIGNED,
-  value INT UNSIGNED COMMENT 'Запас товарной позиции на складе',
+  value INT UNSIGNED COMMENT 'Р—Р°РїР°СЃ С‚РѕРІР°СЂРЅРѕР№ РїРѕР·РёС†РёРё РЅР° СЃРєР»Р°РґРµ',
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) COMMENT = 'Запасы на складе';
+) COMMENT = 'Р—Р°РїР°СЃС‹ РЅР° СЃРєР»Р°РґРµ';
 
 INSERT INTO
   storehouses_products (storehouse_id, product_id, value)
@@ -81,7 +81,7 @@ VALUES
   
 SELECT * FROM storehouses_products ORDER BY value = 0, value;
 
--- Тема Операции, задание 3
+-- РўРµРјР° РћРїРµСЂР°С†РёРё, Р·Р°РґР°РЅРёРµ 3
 
 SELECT *,
 	CASE
@@ -90,29 +90,29 @@ SELECT *,
 	END AS mounth
 FROM users WHERE DATE_FORMAT(birthday_at, "%m") = 5 OR DATE_FORMAT(birthday_at, "%m") = 8;
 
--- Тема Операции, задание 4
+-- РўРµРјР° РћРїРµСЂР°С†РёРё, Р·Р°РґР°РЅРёРµ 4
 
 DROP TABLE IF EXISTS catalogs;
 CREATE TABLE catalogs (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255) COMMENT 'Название раздела',
+  name VARCHAR(255) COMMENT 'РќР°Р·РІР°РЅРёРµ СЂР°Р·РґРµР»Р°',
   UNIQUE unique_name(name(10))
-) COMMENT = 'Разделы интернет-магазина';
+) COMMENT = 'Р Р°Р·РґРµР»С‹ РёРЅС‚РµСЂРЅРµС‚-РјР°РіР°Р·РёРЅР°';
 
 INSERT INTO catalogs VALUES
-  (NULL, 'Процессоры'),
-  (NULL, 'Материнские платы'),
-  (NULL, 'Видеокарты'),
-  (NULL, 'Жесткие диски'),
-  (NULL, 'Оперативная память');
+  (NULL, 'РџСЂРѕС†РµСЃСЃРѕСЂС‹'),
+  (NULL, 'РњР°С‚РµСЂРёРЅСЃРєРёРµ РїР»Р°С‚С‹'),
+  (NULL, 'Р’РёРґРµРѕРєР°СЂС‚С‹'),
+  (NULL, 'Р–РµСЃС‚РєРёРµ РґРёСЃРєРё'),
+  (NULL, 'РћРїРµСЂР°С‚РёРІРЅР°СЏ РїР°РјСЏС‚СЊ');
  
  SELECT * FROM catalogs WHERE id IN (5, 1, 2) ORDER BY  FIELD (id, 5, 1, 2);
 
--- Тема Агрегация, задание 1
+-- РўРµРјР° РђРіСЂРµРіР°С†РёСЏ, Р·Р°РґР°РЅРёРµ 1
 
 SELECT ROUND(AVG((TO_DAYS(NOW()) - TO_DAYS(birthday_at))/365.25), 2) AS AVG_AGE FROM users;
 
--- Тема Агрегация, задание 2
+-- РўРµРјР° РђРіСЂРµРіР°С†РёСЏ, Р·Р°РґР°РЅРёРµ 2
 
 SELECT
 	DAYNAME(CONCAT(YEAR(NOW()),'-', DATE_FORMAT(birthday_at,'%m-%d'))) as week_day,
@@ -122,7 +122,7 @@ FROM
 GROUP BY
 	week_day; 
 
--- Тема Агрегация, задание 3
+-- РўРµРјР° РђРіСЂРµРіР°С†РёСЏ, Р·Р°РґР°РЅРёРµ 3
 
 SELECT EXP(SUM(LOG(id))) as mult FROM catalogs; 
 
